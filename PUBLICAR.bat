922ecfea21e0bee52d5b1309338ce4c2d5c8219f
Roger Franco <francoroger@github>
2026-05-20 00:00:46 -0300

    Atualizacao - 20/05/2026  0:00:40,63

--- PUBLICAR.bat
+++ PUBLICAR.bat
@@ -1,10 +1,6 @@
 @echo off
-REM =====================================================
-REM   PUBLICAR.bat - AUVORATA
-REM   - Self-relaunch em cmd /k (janela NUNCA fecha)
-REM   - Push pro GitHub
-REM   - Chama cpanel-deploy.bat no final
-REM =====================================================
+REM PUBLICAR.bat - AUVORATA
+REM Self-relaunch + push + chama cpanel-deploy.bat
 
 if "%~1"=="STAYOPEN" goto :MAIN
 start "AUVORATA - Publicar" cmd /k call "%~f0" STAYOPEN
@@ -28,21 +24,24 @@ echo Log:   %LOG%
 echo ===============================================
 echo.
 
-REM --- Limpar lock orfao ---
 if exist ".git\index.lock" (
-  echo [SETUP] Removendo lock orfao do git
+  echo [SETUP] Removendo lock orfao
   del /f /q ".git\index.lock" >>"%LOG%" 2>&1
 )
 
-REM --- Limpar arquivos obsoletos ---
+REM --- SEGURANCA: remove do git qualquer cpanel-config*.bat vazado ---
+git ls-files "cpanel-config*.bat" 2>nul | findstr /V "cpanel-config.exemplo.bat" > "%TEMP%\auv_leak.txt"
+for /f "tokens=*" %%G in (%TEMP%\auv_leak.txt) do (
+  echo [SECURITY] Removendo do git: %%G
+  git rm --cached "%%G" >>"%LOG%" 2>&1
+)
+del "%TEMP%\auv_leak.txt" 2>nul
+
+REM --- Limpar obsoletos ---
 set "L=0"
-for %%F in (
-  "images\atelier.svg" "images\hero-piece.svg"
-  "images\piece-aurum.svg" "images\piece-origem.svg" "images\piece-solene.svg"
-  "deploy.bat" "primeiro-setup.bat" "setup-ssh.bat" "SETUP.md"
-) do (
+for %%F in ("images\atelier.svg" "images\hero-piece.svg" "images\piece-aurum.svg" "images\piece-origem.svg" "images\piece-solene.svg" "deploy.bat" "primeiro-setup.bat" "setup-ssh.bat" "SETUP.md") do (
   if exist "%%~F" (
-    if "!L!"=="0" echo [SETUP] Removendo arquivos obsoletos:
+    if "!L!"=="0" echo [SETUP] Removendo obsoletos:
     echo   - %%~F
     del /f /q "%%~F" >>"%LOG%" 2>&1
     set "L=1"
@@ -50,44 +49,38 @@ for %%F in (
 )
 if "!L!"=="1" echo.
 
-echo [1/4] Git instalado?
-where git >nul 2>>"%LOG%" || goto :ERR_GIT
+echo [1/4] Git? & where git >nul 2>>"%LOG%" || ( call :ERR "Git nao instalado." & exit /b 1 )
 echo   [OK]
 echo.
 
-echo [2/4] Repositorio Git presente?
-if not exist ".git" goto :ERR_NOREPO
+echo [2/4] Repo Git? & if not exist ".git" ( call :ERR "Pasta sem repo Git: %CD%" & exit /b 1 )
 echo   [OK]
 echo.
 
-echo [3/4] SSH com GitHub?
-ssh -o BatchMode=yes -o ConnectTimeout=10 -o StrictHostKeyChecking=accept-new -T git@github.com 2>&1 | findstr /C:"successfully authenticated" >nul || goto :ERR_SSH
+echo [3/4] SSH GitHub?
+ssh -o BatchMode=yes -o ConnectTimeout=10 -o StrictHostKeyChecking=accept-new -T git@github.com 2>&1 | findstr /C:"successfully authenticated" >nul || ( call :ERR "SSH com GitHub falhou. Abra Git Bash: ssh -T git@github.com" & exit /b 1 )
 echo   [OK]
 echo.
 
-echo [4/4] Mudancas locais?
+echo [4/4] Mudancas?
 git status --porcelain > "%TEMP%\auv_st.txt" 2>>"%LOG%"
 for %%A in ("%TEMP%\auv_st.txt") do set TAM=%%~zA
 del "%TEMP%\auv_st.txt" 2>nul
-if "%TAM%"=="0" (
-  echo   [INFO] Nenhuma mudanca. Tentando push de commits pendentes.
-  echo.
-  goto :PUSH
-)
-echo   Mudancas detectadas:
+if "%TAM%"=="0" ( echo   [INFO] Nenhuma mudanca local. & echo. & goto :PUSH )
+echo   Detectadas:
 git status --short
 echo.
 
-set "MSG=Atualizacao do site - %date% %time%"
+set "MSG=Atualizacao - %date% %time%"
 echo Mensagem padrao: %MSG%
-set /p "USERMSG=Aperte ENTER ou digite outra: "
+set /p "USERMSG=ENTER ou digite outra: "
 if not "%USERMSG%"=="" set "MSG=%USERMSG%"
 echo.
 
 echo === Commit ===
 git add . 2>>"%LOG%"
-git commit -m "%MSG%" 2>>"%LOG%" || goto :ERR_COMMIT
-echo [OK] Commit criado
+git commit -m "%MSG%" 2>>"%LOG%" || ( call :ERR "Commit falhou. Veja: %LOG%" & exit /b 1 )
+echo [OK] Commit
 echo.
 
 :PUSH
@@ -96,14 +89,12 @@ git push -u origin main 2>>"%LOG%"
 if errorlevel 1 (
   echo [AVISO] Push falhou. Sincronizando...
   git pull origin main --no-edit --allow-unrelated-histories 2>>"%LOG%"
-  git push -u origin main 2>>"%LOG%" || goto :ERR_PUSH
+  git push -u origin main 2>>"%LOG%" || ( call :ERR "Push falhou. Veja: %LOG%" & exit /b 1 )
 )
-echo.
 echo [OK] GitHub atualizado
 echo  https://github.com/francoroger/AUVORATA
 echo.
 
-REM --- Chamar deploy do cPanel ---
 call "%~dp0cpanel-deploy.bat"
 echo.
 
@@ -113,34 +104,10 @@ echo Pressione qualquer tecla para fechar...
 pause >nul
 exit /b 0
 
-:ERR_GIT
-echo [ERRO] Git nao instalado. Baixe em: https://git-scm.com/download/win
+:ERR
+echo.
+echo [ERRO] %~1
+echo.
 echo Pressione qualquer tecla...
 pause >nul
-exit /b 1
-
-:ERR_NOREPO
-echo [ERRO] Esta pasta nao tem repositorio Git: %CD%
-echo Pressione qualquer tecla...
-pause >nul
-exit /b 1
-
-:ERR_SSH
-echo [ERRO] SSH com GitHub nao autenticou.
-echo  Abra Git Bash, rode: ssh -T git@github.com
-echo  Se pedir, digite: yes
-echo Pressione qualquer tecla...
-pause >nul
-exit /b 1
-
-:ERR_COMMIT
-echo [ERRO] Falha no commit. Veja: %LOG%
-echo Pressione qualquer tecla...
-pause >nul
-exit /b 1
-
-:ERR_PUSH
-echo [ERRO] Push falhou. Veja: %LOG%
-echo Pressione qualquer tecla...
-pause >nul
-exit /b 
+goto :eof
